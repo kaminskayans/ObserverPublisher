66f1f1020debf7556b83e16d2020144a4ce03f8c
Nataly <kaminskayans@gmail.com>
2020-06-26 11:02:02 +0500

    Тестовое задание про события

---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+   [SerializeField] Publisher[] publishers;
+    // Start is called before the first frame update
+    void Start()
+    {
+        publishers = Resources.FindObjectsOfTypeAll<Publisher>();
+        if (publishers.Length==10) {
+            ObserverCall();
+        }
+    }
+    public delegate void MethodContainer();
+
+    public static event MethodContainer ObserverCall;
+
+    private static Observer instance;
+
+    private Observer() { }
+
+    public static Observer getInstance() {
+        if (instance == null)
+            instance = new Observer();
+        return instance;
+    }
+
+}
---
+++ Assets/Scripts/Publisher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Publisher : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        Observer.ObserverCall += Message;
+    }
+
+    private void Message() {
+        Debug.Log("Я нашел 10 Publishers");
+    }
+}
